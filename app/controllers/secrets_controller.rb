3e04ccc83247c25ae7c9a6a0b443d68d66fb1e74
Joshua Waiswa <jwaiswa7@gmail.com>
2024-04-05 13:58:10 +0300

    Adding the secret create method

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -3,4 +3,20 @@ class SecretsController < ApplicationController
   def new
     @secret = Secret.new
   end
+
+  def create
+    @secret = Secret.new(secret_params)
+    if @secret.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def secret_params
+    params.require(:secret).permit(:information, :life_time, :password, :password_confirmation)
+  end
 end
